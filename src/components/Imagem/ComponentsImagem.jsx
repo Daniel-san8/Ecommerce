814bac030d13b5a48d59709a5abb2652348989ed
Daniel San <danielgdt88@hotmail.com>
2024-07-16 17:40:04 -0300

    refatoração

---
+++ src/components/Imagem/ComponentsImagem.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledDivImg = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledImg = styled.img`
+  border-radius: 1rem;
+  max-width: 90%;
+  margin: 0 auto;
+  display: block;
+`;
+
+export const StyledDadosImagem = styled.div`
+  margin: 0 1.5rem;
+`;
+
+export const StyledCategory = styled.p`
+  color: gray;
+`;
+
+export const StyledName = styled.p`
+  font-weight: bold;
+`;
+
+export const StyledPreco = styled.p`
+  color: hsl(14, 86%, 42%);
+  font-weight: bolder;
+`;
--- src/components/Imagem/Imagem.jsx
+++ src/components/Imagem/Imagem.jsx
@@ -1,58 +1,36 @@
 import React from "react";
-import styled from "styled-components";
 import { GlobalContext } from "../../GlobalContext";
 import Cart from "../Carrinho/Carrinho";
-
-const StyledImg = styled.img`
-  border-radius: 1rem;
-  max-width: 90%;
-  margin: 0 auto;
-  display: block;
-`;
-
-const StyledDivImg = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledDadosImagem = styled.div`
-  margin: 0 1.5rem;
-`;
-
-const StyledPreco = styled.p`
-  color: hsl(14, 86%, 42%);
-  font-weight: bolder;
-`;
-
-const StyledCategory = styled.p`
-  color: gray;
-`;
+import {
+  StyledCategory,
+  StyledDadosImagem,
+  StyledDivImg,
+  StyledImg,
+  StyledName,
+  StyledPreco,
+} from "./ComponentsImagem";
 
 const Imagem = () => {
   const { dados } = React.useContext(GlobalContext);
   if (dados === null) return null;
   return (
-    <StyledDivImg>
+    <>
       {dados.map(({ image, category, name, price }) => {
         return (
-          <div key={category}>
+          <StyledDivImg key={category}>
             <StyledImg src={image.desktop}></StyledImg>
             {dados && (
               <StyledDadosImagem>
                 {category && <StyledCategory>{category}</StyledCategory>}
-                {category && (
-                  <p>
-                    <strong>{name}</strong>
-                  </p>
-                )}
+                {category && <StyledName>{name}</StyledName>}
                 {category && <StyledPreco>${price.toFixed(2)}</StyledPreco>}
               </StyledDadosImagem>
             )}
-          </div>
+          </StyledDivImg>
         );
       })}
       <Cart />
-    </StyledDivImg>
+    </>
   );
 };
 
