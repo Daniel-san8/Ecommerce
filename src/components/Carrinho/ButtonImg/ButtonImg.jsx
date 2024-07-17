cf305b4b44abce0b30b2b7ff1c2ebb89f95e7f5b
Daniel San <danielgdt88@hotmail.com>
2024-07-17 12:46:39 -0300

    feat: button cart criado

--- src/components/Carrinho/ButtonImg/ButtonImg.jsx
+++ src/components/Carrinho/ButtonImg/ButtonImg.jsx
@@ -6,13 +6,13 @@ export const StyledContainerButtonCart = styled.div`
   width: 50%;
   height: 10%;
   position: absolute;
-  bottom: 115px;
+  bottom: 110px;
   right: 87px;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 1rem;
-  border-radius: 100%;
+  border-radius: 25px;
 `;
 
 export const StyledTextoButtonCart = styled.span`
