620065edd196715dc19e052254667d38b9bacd6c
Daniel San <danielgdt88@hotmail.com>
2024-07-24 15:55:57 -0300

    criação de carrinho com itens

--- src/components/Carrinho/CompononentsCarrinho.jsx
+++ src/components/Carrinho/CompononentsCarrinho.jsx
@@ -39,6 +39,7 @@ export const StyledDivCarrinhoProdutoItens = styled.div`
   flex-direction: column;
   flex: 1;
   gap: 0.8rem;
+  padding: 0 1.5rem;
 `;
 
 export const StyledTituloProduto = styled.span`
@@ -66,4 +67,5 @@ export const StyledSpanDois = styled.span`
 export const StyledImgItens = styled.img`
   width: 20px;
   height: 20px;
+  padding: 0 1.5rem;
 `;
